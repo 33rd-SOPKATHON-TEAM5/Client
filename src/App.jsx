d281e637d2475526d912dc363d5a28c974689f4e
se0jinYoon <tjwls1999@gmail.com>
2023-11-24 00:26:43 +0900

    [init] theme 삭제, api 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import Router from './Router';
 import GlobalStyle from './styles/globalStyle';
-import theme from './styles/Theme';
-import { ThemeProvider } from 'styled-components';
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
+    <>
       <GlobalStyle />
       <Router />
-    </ThemeProvider>
+    </>
   );
 }
 
