0775d237180fb85e437ea9ecbea3297e4ae587eb
Wang HoEun <hoeunwang@gmail.com>
2023-11-25 23:26:20 +0900

    fix: 모바일 뷰 삭제

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,32 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-// import Main from '../../pages/Main/Main';
-import { styled } from "styled-components";
 import MainTest from "./components/main/MainTest";
 
 const Router = () => {
   return (
     <BrowserRouter>
-      <Wrapper>
-        <Routes>
-          <Route path="/" element={<MainTest />} />
-        </Routes>
-      </Wrapper>
+      <Routes>
+        <Route path="/" element={<MainTest />} />
+      </Routes>
     </BrowserRouter>
   );
 };
-
-const Wrapper = styled.div`
-  max-width: 425px;
-  width: 100vw;
-  min-height: 100vh;
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  justify-content: flex-start;
-  background-color: red;
-  word-break: break-all;
-`;
 
 export default Router;
