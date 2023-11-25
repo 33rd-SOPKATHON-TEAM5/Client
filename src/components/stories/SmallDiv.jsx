2f045dbc1bc31de6d04fc5be48722876d2d6f690
se0jinYoon <tjwls1999@gmail.com>
2023-11-26 03:22:04 +0900

    feat: small 카드 정렬

--- src/components/stories/SmallDiv.jsx
+++ src/components/stories/SmallDiv.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import {SmallCardImage} from '../../assets/Svgs';
+import { SmallCardImage } from '../../assets/Svgs';
 
-
+// content에 가져온 데이터
+// 닉네임에 get 해온 유저닉네임
 const SmallDiv = () => {
   return (
     <SmallContainer>
-      <Content>4년만난 남자친구랑 헤어져서 울었어요ㅠㅠㅠ</Content>
-      <UserNickName>아이디</UserNickName>
+      <ContentDiv>
+        <Content>4년만난 남자친구랑 헤어져서 울었어요ㅠㅠㅠ</Content>
+          <UserNickName>닉네임</UserNickName>
+      </ContentDiv>
     </SmallContainer>
   );
 };
@@ -16,18 +19,30 @@ const SmallDiv = () => {
 export default SmallDiv;
 
 const SmallContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
   width: 100%;
   height: 10.6rem;
+  padding: 2.8rem 2.4rem;
   border-radius: 0.6rem;
-  background-color: grey;
-  padding: 1rem 2rem;
   background-image: url(${SmallCardImage});
+`;
+
+const ContentDiv = styled.div`
+position: relative;
+  height: 5rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 const Content = styled.div``;
 
-const UserNickName = styled.span``;
+const NicknameContainer = styled.div`
+`
+
+const UserNickName = styled.div`
+position: absolute;
+bottom: 0;
+right: 0;
+
+`;
